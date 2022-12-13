689daaf0db9d75034f41b733d921c52122d40d35
Sapan Patibandha <sapan.patibandha@contis.com>
2022-12-13 20:29:33 +0530

    bug fixing

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -20,10 +19,13 @@ func PublishMessage(c context.Context, api SNSPublishAPI, input *sns.PublishInpu
 }
 
 func main() {
-	msg := flag.String("m", "", "The message to send to the subscribed users of the topic")
-	topicARN := flag.String("t", "", "The ARN of the topic to which the user subscribes")
+	// msg := flag.String("m", "", "The message to send to the subscribed users of the topic")
+	// topicARN := flag.String("t", "", "The ARN of the topic to which the user subscribes")
 
-	flag.Parse()
+	msg := aws.String("test message")
+	topicARN := aws.String("arn:aws:sns:us-west-2:000000000000:trial-proj1-sns")
+
+	// flag.Parse()
 
 	if *msg == "" || *topicARN == "" {
 		fmt.Println("You must supply a message and topic ARN")
@@ -33,21 +35,43 @@ func main() {
 	cfg := aws.Config{
 		EndpointResolver: aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
 			return aws.Endpoint{
-				PartitionID:       "aws",
-				URL:               "http://localhost:4566",
-				SigningRegion:     "us-west-2",
-				HostnameImmutable: true,
+				PartitionID:   "aws",
+				URL:           "http://localhost:4566",
+				SigningRegion: "us-west-2",
+				// HostnameImmutable: false,
 			}, nil
 		}),
 	}
 
 	client := sns.NewFromConfig(cfg)
-	input := &sns.PublishInput{
-		Message:  msg,
-		TopicArn: topicARN,
-	}
+	// input := &sns.PublishInput{
+	// 	Message:          msg,
+	// 	TopicArn:         topicARN,
+	// 	MessageStructure: aws.String("json"),
+	// }
 
-	result, err := PublishMessage(context.TODO(), client, input)
+	// // List SNS
+	// result, err := client.Subscribe(context.TODO(), &sns.SubscribeInput{
+	// 	Endpoint:              aws.String("sapan.patibandha@contis.com"),
+	// 	Protocol:              aws.String("email"),
+	// 	ReturnSubscriptionArn: *aws.Bool(true), // Return the ARN, even if user has yet to confirm
+	// 	TopicArn:              topicARN,
+	// })
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Println(*result.SubscriptionArn)
+
+	// result, err := PublishMessage(context.TODO(), client, input)
+	result, err := client.Publish(context.TODO(), &sns.PublishInput{
+		Message:          msg,
+		TopicArn:         topicARN,
+		MessageStructure: aws.String("json"),
+	})
+
 	if err != nil {
 		fmt.Println("Got an error publishing the message:")
 		fmt.Println(err)
