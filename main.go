bc5bd060b7a1134fc5d5fcc1892c23386f81c5a0
Sapan Patibandha <sapan.patibandha@contis.com>
2022-12-27 16:49:12 +0530

    connected to localstack and S3 list completed

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 )
 
@@ -24,26 +27,39 @@ func main() {
 
 	msg := aws.String("test message")
 	topicARN := aws.String("arn:aws:sns:us-west-2:000000000000:trial-proj1-sns")
-
 	// flag.Parse()
+
+	awsEndpoint := "http://localhost:4566"
+	awsRegion := "us-east-1"
 
 	if *msg == "" || *topicARN == "" {
 		fmt.Println("You must supply a message and topic ARN")
 		fmt.Println("-m MESSAGE -t TOPIC-ARN")
 		return
 	}
-	cfg := aws.Config{
-		EndpointResolver: aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+
+	customResolver := aws.EndpointResolverFunc(func(service, region string) (aws.Endpoint, error) {
+		if awsEndpoint != "" {
 			return aws.Endpoint{
 				PartitionID:   "aws",
-				URL:           "http://localhost:4566",
-				SigningRegion: "us-west-2",
-				// HostnameImmutable: false,
+				URL:           awsEndpoint,
+				SigningRegion: awsRegion,
 			}, nil
-		}),
+		}
+
+		// returning EndpointNotFoundError will allow the service to fallback to its default resolution
+		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+	})
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(awsRegion),
+		config.WithEndpointResolver(customResolver),
+	)
+	if err != nil {
+		log.Fatalf("Cannot load the AWS configs: %s", err)
 	}
 
-	client := sns.NewFromConfig(cfg)
+	// client := sns.NewFromConfig(cfg)
+
 	// input := &sns.PublishInput{
 	// 	Message:          msg,
 	// 	TopicArn:         topicARN,
@@ -66,17 +82,36 @@ func main() {
 	// fmt.Println(*result.SubscriptionArn)
 
 	// result, err := PublishMessage(context.TODO(), client, input)
-	result, err := client.Publish(context.TODO(), &sns.PublishInput{
-		Message:          msg,
-		TopicArn:         topicARN,
-		MessageStructure: aws.String("json"),
+	// result, err := client.Publish(context.TODO(), &sns.PublishInput{
+	// 	Message:          msg,
+	// 	TopicArn:         topicARN,
+	// 	MessageStructure: aws.String("json"),
+	// })
+
+	// if err != nil {
+	// 	fmt.Println("Got an error publishing the message:")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println("Message ID: " + *result.MessageId)
+
+	//--------------------------------------------------------------------
+
+	// Create the resource client
+	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.UsePathStyle = true
 	})
 
+	buckets, err := client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
 	if err != nil {
-		fmt.Println("Got an error publishing the message:")
-		fmt.Println(err)
+		fmt.Printf("Couldn't list buckets: %v", err)
 		return
 	}
 
-	fmt.Println("Message ID: " + *result.MessageId)
+	for _, bucket := range buckets.Buckets {
+		fmt.Printf("Found bucket: %s, created at: %s\n", *bucket.Name, *bucket.CreationDate)
+	}
+
+	//--------------------------------------------------------------------
 }
